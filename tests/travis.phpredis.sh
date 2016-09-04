806ba0a2ef1bfd5ebcb560c10f48f7baddba564b
Filip Procházka <filip@prochazka.su>
2016-09-04 21:23:26 +0200

    Travis: fix redis build

--- tests/travis.phpredis.sh
+++ tests/travis.phpredis.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-mkdir ~/build-phpredis && cd ~/build-phpredis || exit 1
+mkdir /tmp/build-phpredis && cd /tmp/build-phpredis || exit 1
 
-wget -O phpredis.zip "https://github.com/nicolasff/phpredis/archive/master.zip" && unzip phpredis.zip && cd phpredis-master/ || exit 1
+if [ "$TRAVIS_PHP_VERSION" == "7.0" ]; then
+	PHP_REDIS_BRANCH="php7"
+else
+	PHP_REDIS_BRANCH="master"
+fi
+
+wget -O phpredis.zip "https://github.com/phpredis/phpredis/archive/$PHP_REDIS_BRANCH.zip" && unzip phpredis.zip && cd phpredis-*/ || exit 1
 echo
 
-phpize && ./configure && make && sudo make install || exit 1
+phpize && ./configure && make && make install || exit 1
 echo
 
 echo 'extension = redis.so' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
